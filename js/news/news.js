ab39fb5d9368f5edefe3a89970c45218c32c467d
zhangbing <bingzh1024@163.com>
2018-08-10 10:40:44 +0800

    剧目

--- js/news/news.js
+++ js/news/news.js
@@ -4,4 +4,16 @@ $(document).ready(function () {
 		$(this).addClass('active').siblings('.nav-item').removeClass('active');
 		$('.nav-content-wrap .nav-content').eq(index).addClass('active').siblings('.nav-content').removeClass('active');
 	});
+
+	// 返回的json格式数据
+	// {
+	//    "code": 200,
+	//    "msg": "成功",
+	//    "data": {
+	//       "idType": 0,
+	//       "sex": "男",
+	//       "idTypeEnum": "身份证",
+	//       "idNum": ""
+	//    }
+	// }
 });
