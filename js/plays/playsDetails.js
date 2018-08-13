3266b0bb46a645b1481beba3003897e5ecc5c55b
zhangbing <bingzh1024@163.com>
2018-08-14 00:06:05 +0800

    getdetail

--- js/plays/playsDetails.js
+++ js/plays/playsDetails.js
@@ -7,4 +7,27 @@ var newsSwiper = new Swiper('#creatorsSwiper',{
     slidesPerView :'auto',
     // spaceBetween : (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth)* (100 / 750)*0.18,
     freeMode: true,
-})
+});
+// 获取当前详情页数据
+getDetail();
+
+/*获取当前页详情*/
+function getDetail() {
+    $.ajax({
+        url: BASEURL + 'detail/' + getParams('id'),
+        type: 'GET',
+        dataType: 'json',
+        // data: {param1: 'value1'},
+        success: function (data) {
+            if (data.code != 200) {
+                layerAlert(data.msg);
+            }else {
+                // renderPlaysList(data.data);
+            }
+        },
+        error: function (err) {
+            layerAlert('系统繁忙，请稍后再试');
+        }
+    })
+    
+}
