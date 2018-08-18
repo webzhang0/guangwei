f4c05465d279e97bef63d875e1edd7669fe55baf
zhangbing <bingzh1024@163.com>
2018-08-18 15:01:30 +0800

    allreport

--- js/plays/allReport.js
+++ js/plays/allReport.js
@@ -4,21 +4,46 @@ getAllReport("1");
 function getAllReport(pageNum) {
 	$.ajax({
 		url: BASEURL + 'detail/article/'+getParams('id'),
-		type: 'POST',
-		data: {
-			// "id": "27",
-			"pageSize": "10",
-			"pageNum": pageNum,
-		},
-		success: function () {
+		// type: 'POST',
+		// data: {
+		// 	// "id": "27",
+		// 	"pageSize": "10",
+		// 	"pageNum": pageNum,
+		// },
+		success: function (data) {
 			if (data.code != 200) {
                 layerAlert(data.msg);
             }else {
-
+            	renderAllReport(data.data);
             }
 		},
 		error: function () {
 			layerAlert('系统繁忙，请稍后再试');
 		}
 	})
+}
+// 渲染
+function renderAllReport(data) {
+	var reportsHtml = '';
+    for(var n = 0; n < data.length; n++) {
+        reportsHtml += '<li class="report-item">'
+            +'<a href="javascript:;" class="clearfix report-a">'
+            +'<img src="'+ data[n]['articleBanner'] + '" class="report-img fl-l">'
+            +'<div class="fl-r report-item-inf">'
+            +'<h3 class="report-item-title">' + data[n]['articleTitle'] + '</h3>'
+
+            if(data[n]['articleIntroduce'].length > 25) {
+                reportsHtml +='<p class="report-item-con">' + data[n]['articleIntroduce'].substr(0, 25) +'...<span class="see-all">查看全部></span></p>'
+            }else {
+                reportsHtml +='<p class="report-item-con">' + data[n]['articleIntroduce'] +'</p>'
+            }
+
+        reportsHtml +='<p class="report-src-wrap"><span class="report-src">光纬戏剧</span><span class="report-date">'+ formatterDate(data[n]['createDateTime']) +'</span></p>'
+            +'</div></a></li>'
+    }
+    $('#reports').html(reportsHtml);
+    $('#reports').on('click', '.see-all', function() {
+        var index = $(this).closest('li').index();
+        $(this).parent('.report-item-con').html(data[index]['articleIntroduce']);
+    });
 }
